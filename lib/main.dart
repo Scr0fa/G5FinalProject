4552d3f640b11b55ef8838c299cd2311d636cd98
Lloyd Anthony Salas <106375944+Tensa11@users.noreply.github.com>
2023-01-19 16:01:47 +0800

    Updating Main dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        resizeToAvoidBottomInset: false,
         body: SingleChildScrollView(
           child: LandingPage(),
         ),
